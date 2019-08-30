0189c465dad0e6611789c4a8a198b7f2c1f5db97
k-lauer <54539807+k-lauer@users.noreply.github.com>
2019-08-30 08:14:08 -0500

    Title change

---
+++ abc.py
@@ -0,0 +1,27 @@
+print("hello world!")
+name = "kate Lauer"
+print(name)
+
+teacher_name = name 
+print(teacher_name)
+
+
+print(dir(name))
+
+name = "test subject"
+
+# Single Line Comment 
+"""
+This is a multi line comment
+print()
+"""
+
+print("I'm not in a comment")
+
+
+a = 1
+b = 2
+
+print(a+b, "addition") # Addition
+print(b-a, "subtraction") # Subtraction 
+Print(b/a, "division") # Division 
