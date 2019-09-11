11dc43be05dad0633f590cef06b369539ce60246
k-lauer <54539807+k-lauer@users.noreply.github.com>
2019-09-11 08:29:09 -0500

    web programming

---
+++ Week2_2.py
@@ -0,0 +1,98 @@
+"""
+print("What is your favorite number?")
+favrorite_number = input("What is your favorite number?") 
+
+num_1 = int(input("Input number 1: "))
+num_2 = int(input("input number 2: "))
+
+"1 + 1 = 2"
+
+print(str9num_1) + "+" + str(num_2) + "=" 
+
+"""
+
+
+
+import turtle  # need to import the module
+
+screen = turtle.Screen()  # need to create a new screen/drawing board
+screen.bgcolor("red") # seets background color 
+screen.title("CIS-121")
+
+drawing_turtle = turtle.Turtle() # need to create a turtle to draw with
+
+drawing_turtle.forward(100)
+drawing_turtle.left(60) 
+
+drawing_turtle.forward(100)
+drawing_turtle.left(60) 
+
+drawing_turtle.forward(100)
+drawing_turtle.left(60) 
+
+drawing_turtle.forward(100)
+drawing_turtle.left(60) 
+
+drawing_turtle.forward(100)
+drawing_turtle.left(60) 
+
+drawing_turtle.forward(100)
+drawing_turtle.left(60) 
+
+drawing_turtle.right(60)
+drawing_turtle.forward(100)
+
+drawing_turtle.left(120)
+drawing_turtle.forward(100)
+
+drawing_turtle.right(60)
+drawing_turtle.forward(100)
+
+drawing_turtle.left(120)
+drawing_turtle.forward(100)
+
+drawing_turtle.right(60)
+drawing_turtle.forward(100)
+
+drawing_turtle.left(120)
+drawing_turtle.forward(100)
+
+drawing_turtle.right(60)
+drawing_turtle.forward(100)
+
+drawing_turtle.left(120)
+drawing_turtle.forward(100)
+
+drawing_turtle.right(60)
+drawing_turtle.forward(100)
+
+drawing_turtle.left(120)
+drawing_turtle.forward(100)
+
+drawing_turtle.right(60)
+drawing_turtle.forward(100)
+
+drawing_turtle.left(120)
+drawing_turtle.forward(100)
+
+
+
+
+
+turtle.done()  # this keeps screen open till you close it 
+
+
+
+"""
+import turtle
+import time
+import random
+
+window = turtle.Screen()
+drawing_turtle = turtle.Turtle()
+
+num_sides = input("enter the number of sides of the shape you want. ")
+
+if num_sides.isdigit()
+    num_sides = int(num_)
+"""
