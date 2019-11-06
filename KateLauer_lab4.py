1ecc78938be032846375e47692900969e56be97b
k-lauer <54539807+k-lauer@users.noreply.github.com>
2019-11-06 07:45:25 -0600

    Update KateLauer_lab4.py

--- KateLauer_lab4.py
+++ KateLauer_lab4.py
@@ -10,7 +10,22 @@ def get_shift():
         print('Invalid Number')
         return get_shift()
 
-file1 = open('KateLauer_original.txt')
-read_file1 = file1.read()
-
 alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',)
+
+if __name__ == "__main__"
+    file1 = open('KateLauer_original.txt')
+    read_file1 = file1.readlines()
+
+    for line in file1:
+        words = line.split()
+        encrypted_line = []
+        for word in words:
+            encrypted_word = ""
+            for char in word.lower()
+                encrypted_word += chr((ord(letter)+shift -97))%26+97
+
+            enctrypted_line.append
+
+        encrypted_file.write(' ',join(encrypted_line))
+
+
